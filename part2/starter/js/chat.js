6281e3dec2d713d998228f86b5df25a044678b66
Jonathan Cady <joncady@uw.edu>
2020-04-27 22:03:06 -0700

    part 2 prep

---
+++ part2/starter/js/chat.js
@@ -0,0 +1,47 @@
+// data to render
+const chats = [
+    {
+        name: "Charlie",
+        pic: "img/tony.JPG",
+        lastMessage: "Your suit looks so great!",
+        from: true,
+        unread: false
+    },
+    {
+        name: "Kevin",
+        pic: "img/legolas.JPG",
+        lastMessage: "Thanks dude!",
+        from: false,
+        unread: true
+    },
+    {
+        name: "Emma",
+        pic: "img/widow.jfif",
+        lastMessage: "Wow that's really impresssive!",
+        from: false,
+        unread: true
+    },
+    {
+        name: "Naruto",
+        pic: "img/naruto.JPG",
+        lastMessage: "You look great!",
+        from: true,
+        unread: false
+    }
+];
+
+/**
+ * renders all of the chats onto the page
+ * @param {list of chats to render} chats 
+ */
+function renderChats(chats) {
+}
+
+/**
+ * takes in one chat object and returns it as HTML
+ * @param {object that represents a chat} chatObj 
+ */
+function renderOneChat(chatObj) {
+}
+
+// call the function to render chats
