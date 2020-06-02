979b4d914f3803b70597b27186b52c54dae51770
Jonathan Cady <joncady@uw.edu>
2020-06-01 21:07:44 -0700

    remove creds

--- part6/starter/src/index.js
+++ part6/starter/src/index.js
@@ -8,13 +8,7 @@ import 'firebase/database';
 import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBNsrlV82JVelHAaHKGqEJSnYpu-bsnTb0",
-    authDomain: "lab-demo-1a476.firebaseapp.com",
-    databaseURL: "https://lab-demo-1a476.firebaseio.com",
-    projectId: "lab-demo-1a476",
-    storageBucket: "lab-demo-1a476.appspot.com",
-    messagingSenderId: "1094517096864",
-    appId: "1:1094517096864:web:ccb4f8aa9bba4256df64c5"
+    // add in cred
 };
 
 firebase.initializeApp(firebaseConfig);
